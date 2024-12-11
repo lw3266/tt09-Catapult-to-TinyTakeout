f7561016835981fce0d5376632cb7a0fccd617af
JuanGGil <jgg8979@nyu.edu>
2024-12-11 14:27:22 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -2392,3 +2392,34 @@ module fir (
 endmodule
 
 
+
+module tt_um_fir (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // Internal Signals
+  wire [31:0] a;
+  wire [31:0] b;
+  wire c0,c1,rst;
+
+  assign a[7:0] = ui_in[7:0]; 
+
+
+  
+  fir fir_inst (
+      .clk(clk),
+    .rst(rst),
+    .y_rsc_dat(b),
+    .y_triosy_lz(c0),
+    .x_rsc_dat(a),
+    .x_triosy_lz(c1)
+  );
+
+
