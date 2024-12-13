cf09eba03167c5d24af3ce6944b607700a249f52
JuanGGil <jgg8979@nyu.edu>
2024-12-13 16:29:05 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,7 +27,7 @@ async def test_project(dut):
 
     expected = [0, -95, -190, 86, 362, 38, -286, 390, 1066, 1142, 1218, 1665, 2112, 2464, 2816, 3168, 3520, 3872, 4224, 4576, 4928, 5280, 5632, 5984, 6336, 6688, 7040, 7392, 7744, 8096]
 
-    for i in range(0,30):        
+    for i in range(0,256):        
         dut.ui_in.value = i
         await ClockCycles(dut.clk, 10)
         x = ((dut.uo_out.value << 8) + dut.uio_out.value)
