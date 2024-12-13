4711c420792756fae526359d27dd63b2139f2044
JuanGGil <jgg8979@nyu.edu>
2024-12-13 16:50:15 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -27,13 +27,16 @@ async def test_project(dut):
 
     expected = [0, -95, -190, 86, 362, 38, -286, 390, 1066, 1142, 1218, 1665, 2112, 2464, 2816, 3168, 3520, 3872, 4224, 4576, 4928, 5280, 5632, 5984, 6336, 6688, 7040, 7392, 7744, 8096]
 
-    for i in range(1,100):
-        dut.ui_in.value = 1
-        await ClockCycles(dut.clk, i)
-        x = ((dut.uio_out.value << 8) + dut.uo_out.value)
-        dut._log.info(f"input: {dut.ui_in.value} Clock = {i} --> Output: {dut.uio_out.value}{dut.uo_out.value} (dec:{x}) Expected: {expected[1]} or 1111 1111 1010 0001")
+    # for i in range(1,100):
+    #     dut.ui_in.value = 1
+    #     await ClockCycles(dut.clk, i)
+    #     x = ((dut.uio_out.value << 8) + dut.uo_out.value)
+    #     dut._log.info(f"input: {dut.ui_in.value} Clock = {i} --> Output: {dut.uio_out.value}{dut.uo_out.value} (dec:{x}) Expected: {expected[1]} or 1111 1111 1010 0001")
 
-    
+    for i in range(0,19):
+        await ClockCycles(dut.clk, 80)
+        dut._log.info(f"Output: {dut.uio_out.value}{dut.uo_out.value} (dec:{x}).") # Expected: {expected[i]}")
+
     
     for i in range(0,256):        
         dut.ui_in.value = i
