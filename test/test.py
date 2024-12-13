23ebcf525e779f4e86f116ee1de6df997d2ad65b
JuanGGil <jgg8979@nyu.edu>
2024-12-13 15:15:11 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -25,36 +25,15 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # for i in range (0,256):
-    #     for j in range(0,256):
-    #         # set input vals
-    #         dut.ui_in.value = i
-    #         dut.uio_in.value = j
-
-    #         # Wait for three clock cycle to see the output values
-    #         # await ClockCycles(dut.clk, 3)
-
-    #         # The following assersion is just an example of how to check the output values.
-    #         # Change it to match the actual expected output of your module:
-    #         assert dut.uo_out.value == (i+j)%256
-
     expected = [0, -95, -190, 86, 362, 38, -286, 390, 1066, 1142, 1218, 1665, 2112, 2464, 2816, 3168, 3520, 3872, 4224, 4576, 4928, 5280, 5632, 5984, 6336, 6688, 7040, 7392, 7744, 8096]
-    # expected = [0, 0, 0, 0]
 
-    for i in range(0,30):
-            # Reset
-        dut._log.info("Reset")
-        dut.ena.value = 1
-        dut.ui_in.value = i
-        dut.uio_in.value = 0
-        dut.rst_n.value = 1
-        await ClockCycles(dut.clk, 10)
-        dut.rst_n.value = 0
-        
+    for i in range(0,30):        
         dut.ui_in.value = i
         await ClockCycles(dut.clk, 10)
+        dut._log.info(f"SUB case {i}. a: {a}, b: {b}, input: {dut.ui_in.value}, output: {dut.uo_out.value}, UIO: {dut.uio_out.value}")
 
-        print(dut.uio_out.value, dut.uo_out.value)
+        dut._log.info(f"input: {dut.ui_in.value} --> Output: {dut.uo_out.value}{dut.uio_out.value}")
+        #print(dut.ui_in.value, "-->" ,dut.uio_out.value, dut.uo_out.value)
         #assert( (int(dut.uio_out.value) + int(dut.uo_out.value)) == expected[i])
 
     # Keep testing the module by changing the input values, waiting for
